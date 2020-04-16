024b2394f8cd2c9a1f743c15edfe9e310e4e5ac3
Zach Gorman <gorman.zacharyj@gmail.com>
2020-04-15 22:06:37 -0500

    changed shebang for portability and removed heart stuff

--- .tmux/scripts/battery.sh
+++ .tmux/scripts/battery.sh
@@ -1,61 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Stolen from https://github.com/richoH/dotfiles/blob/master/bin/battery
 # then edited to display percent if charging, and hearts if not
 # NOTE: While I primarily use macOS, I've left in the Linux and FreeBSD
 # implementations for compatability
 
-#CHRG_STUS=`pmset -g batt | grep -o "discharging"`
-#if [ ! -z "$CHRG_STUS" ]; then
-#  CUTE_BATTERY_INDICATOR=1
-#fi
-
-HEART_FULL=♥
-HEART_EMPTY=♡
-[ -z "$NUM_HEARTS" ] &&
-    NUM_HEARTS=5
-
-cutinate()
-{
-    perc=$1
-    inc=$(( 100 / $NUM_HEARTS))
-
-    NUM_EMPTY=0
-    NUM_FULL=0
-    for i in `seq $NUM_HEARTS`; do
-        if [ $perc -lt 100 ]; then
-            ((NUM_EMPTY++)) #echo $HEART_EMPTY
-        else
-            ((NUM_FULL++)) #echo $HEART_FULL
-        fi
-        perc=$(( $perc + $inc ))
-    done
-    while [ $NUM_FULL -gt 0 ]
-    do
-      echo $HEART_FULL
-      ((NUM_FULL--))
-    done
-    while [ $NUM_EMPTY -gt 0 ]
-    do
-      echo $HEART_EMPTY
-      ((NUM_EMPTY--))
-    done
-}
-
-linux_get_bat ()
-{
+linux_get_bat () {
     bf=$(cat $BAT_FULL)
     bn=$(cat $BAT_NOW)
     echo $(( 100 * $bn / $bf ))
 }
 
-freebsd_get_bat ()
-{
+freebsd_get_bat () {
     echo "$(sysctl -n hw.acpi.battery.life)"
-
 }
 
-battery_status()
-{
+battery_status() {
 case $(uname -s) in
     "Linux")
         BATPATH=/sys/class/power_supply/BAT0
@@ -117,9 +76,4 @@ esac
 
 BATTERY_STATUS=`battery_status $1`
 [ -z "$BATTERY_STATUS" ] && exit
-
-if [ -n "$CUTE_BATTERY_INDICATOR" ]; then
-    echo `cutinate $BATTERY_STATUS`
-else
-    echo ${BATTERY_STATUS}%
-fi
+echo ${BATTERY_STATUS}%
