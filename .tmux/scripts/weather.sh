d3134051f1212fb636410483ee3d007fd64f0bf3
Zach Gorman <gorman.zacharyj@gmail.com>
2020-04-19 03:19:33 -0500

    wasn't behaving so fixed

--- .tmux/scripts/weather.sh
+++ .tmux/scripts/weather.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-# Uses wttr.in to get weather in current location. Might only works on macOS.
+# Uses wttr.in to get weather in current location.
 
-curl "wttr.in/?format=3&u" 2>/dev/null | xargs
+curl "wttr.in/?format=%l:+%c+%t&u" 2>/dev/null | cut -d":" -f 2 | xargs
 
 # If your tmux status line doesn't support emojis
 # curl "wttr.in/?format=%l:+%C+%t&u" 2>/dev/null | xargs
